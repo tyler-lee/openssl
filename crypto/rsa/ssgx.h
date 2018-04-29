b8571c5876467078a1be9b7ca855c0dc680da096
Tyler-Lee <lihuorong@iie.ac.cn>
2018-04-29 10:47:08 +0800

    enable show

--- crypto/rsa/ssgx.h
+++ crypto/rsa/ssgx.h
@@ -5,6 +5,7 @@
 int local_sched_policy_priority_save(int new_policy, int new_priority, int* policy, int* priority);
 int local_sched_policy_priority_restore(int policy, int priority);
 
+#define SSGX_DEBUG
 #ifdef SSGX_DEBUG
 void show_thread_policy_and_priority();
 #endif	//! SSGX_DEBUG
